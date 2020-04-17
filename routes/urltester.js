6493bd3093fc172f6908412c4b06fd7c95fb26a6
Uzair Ashraf <uzinatorcl@live.com>
2020-04-17 00:46:11 -0700

    url tester complete, moving to embed link to page

--- routes/urltester.js
+++ routes/urltester.js
@@ -1,10 +1,28 @@
 const route = require('express-promise-router')()
 const fetch = require('node-fetch')
+const ClientError = require('../services/errorhandling')
+
 route
   .post('/', async (req, res, next) => {
     const { url } = req.body
-    console.log(url)
-    res.json({ url })
+    const tests = {
+      youtube: /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/,
+      spotify: /(https?:\/\/open.spotify.com\/(track|user|artist|album)\/[a-zA-Z0-9]+(\/playlist\/[a-zA-Z0-9]+|)|spotify:(track|user|artist|album):[a-zA-Z0-9]+(:playlist:[a-zA-Z0-9]+|))/
+    }
+    let response = null
+    if (tests.youtube.test(url)) {
+      response = {
+        provider: 'youtube'
+      }
+    } else if (tests.spotify.test(url)) {
+      response = {
+        provider: 'spotify'
+      }
+    } else {
+      throw new ClientError('Invalid URL', 406)
+    }
+    response.url = url
+    res.json(response)
   })
 
 module.exports = route
